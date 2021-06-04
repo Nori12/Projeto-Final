4f49f529fdd4e2585494b6657fd51af94bbd03f1
Nori <phbnori@gmail.com>
2021-06-04 10:39:06 -0300

    Bug fixed: creating only one 1 year of weekly candles per ticker.

--- src/db_model.py
+++ src/db_model.py
@@ -89,20 +89,20 @@ class DBTickerModel:
 
         self.insert_update(query)
 
-    def insert_hourly_candles(self, ticker, data):
+    # def insert_hourly_candles(self, ticker, data):
 
-        number_of_rows = len(data)
-        query = 'INSERT INTO hourly_candles (ticker, date_hour, open_price, max_price, min_price, close_price, volume)\nVALUES\n'
+    #     number_of_rows = len(data)
+    #     query = 'INSERT INTO hourly_candles (ticker, date_hour, open_price, max_price, min_price, close_price, volume)\nVALUES\n'
 
-        for n, (index, row) in enumerate(data.iterrows()):
-            if n == number_of_rows - 1:
-                query += f"""('{ticker}', '{index.strftime('%Y-%m-%d %H:%M:%S')}', {row['Open']:.6f}, {row['High']:.6f}, {row['Low']:.6f}, {row['Close']:.6f}, {row['Volume']:.0f})\n"""
-            else:
-                query += f"""('{ticker}', '{index.strftime('%Y-%m-%d %H:%M:%S')}', {row['Open']:.6f}, {row['High']:.6f}, {row['Low']:.6f}, {row['Close']:.6f}, {row['Volume']:.0f}),\n"""
+    #     for n, (index, row) in enumerate(data.iterrows()):
+    #         if n == number_of_rows - 1:
+    #             query += f"""('{ticker}', '{index.strftime('%Y-%m-%d %H:%M:%S')}', {row['Open']:.6f}, {row['High']:.6f}, {row['Low']:.6f}, {row['Close']:.6f}, {row['Volume']:.0f})\n"""
+    #         else:
+    #             query += f"""('{ticker}', '{index.strftime('%Y-%m-%d %H:%M:%S')}', {row['Open']:.6f}, {row['High']:.6f}, {row['Low']:.6f}, {row['Close']:.6f}, {row['Volume']:.0f}),\n"""
 
-        query += 'ON CONFLICT ON CONSTRAINT hourly_data_pkey DO NOTHING;'
+    #     query += 'ON CONFLICT ON CONSTRAINT hourly_data_pkey DO NOTHING;'
 
-        self.insert_update(query)
+    #     self.insert_update(query)
 
     def upsert_splits(self, ticker, data):
 
@@ -149,103 +149,103 @@ class DBTickerModel:
 
         self.insert_update(query)
 
-    def update_hourly_candles_with_split(self, ticker):
+    # def update_hourly_candles_with_split(self, ticker):
 
-        # Update by comparing to daily_candles prices
-        query = f'UPDATE hourly_candles hc\nSET\n'
-        query += f"""  open_price = ROUND(open_price / n.norm_ratio, 2),\n"""
-        query += f"""  max_price = ROUND(max_price / n.norm_ratio, 2),\n"""
-        query += f"""  min_price = ROUND(min_price / n.norm_ratio, 2),\n"""
-        query += f"""  close_price = ROUND(close_price / n.norm_ratio, 2)\n"""
-        query += f"""FROM\n"""
-        query += f"""  (SELECT q.ticker, q.date_hour,\n"""
-        query += f"""    CASE\n"""
-        query += f"""    WHEN ROUND(q.max_price / dc.max_price, 1) >= 0.9 THEN ROUND(q.max_price / dc.max_price, 0)\n"""
-        query += f"""    WHEN ROUND(dc.max_price / q.max_price, 1) >= 0.9 THEN ROUND(1 / ROUND(dc.max_price / q.max_price, 0), 6)\n"""
-        query += f"""    ELSE 1 END AS norm_ratio\n"""
-        query += f"""  FROM daily_candles dc\n"""
-        query += f"""  INNER JOIN\n"""
-        query += f"""    (SELECT ticker, MIN(date_hour) AS date_hour, MAX(max_price) AS max_price\n"""
-        query += f"""    FROM hourly_candles\n"""
-        query += f"""    GROUP BY ticker, DATE(date_hour)) q\n"""
-        query += f"""  ON q.ticker = dc.ticker\n"""
-        query += f"""    AND DATE(dc.day) = DATE(q.date_hour)\n"""
-        query += f"""  ) n\n"""
-        query += f"""WHERE\n"""
-        query += f"""  n.ticker = \'{ticker}\'\n"""
-        query += f"""  AND n.ticker = hc.ticker\n"""
-        query += f"""  AND DATE(n.date_hour) = DATE(hc.date_hour)\n"""
-        query += f"""  AND n.norm_ratio <> 1;\n"""
+    #     # Update by comparing to daily_candles prices
+    #     query = f'UPDATE hourly_candles hc\nSET\n'
+    #     query += f"""  open_price = ROUND(open_price / n.norm_ratio, 2),\n"""
+    #     query += f"""  max_price = ROUND(max_price / n.norm_ratio, 2),\n"""
+    #     query += f"""  min_price = ROUND(min_price / n.norm_ratio, 2),\n"""
+    #     query += f"""  close_price = ROUND(close_price / n.norm_ratio, 2)\n"""
+    #     query += f"""FROM\n"""
+    #     query += f"""  (SELECT q.ticker, q.date_hour,\n"""
+    #     query += f"""    CASE\n"""
+    #     query += f"""    WHEN ROUND(q.max_price / dc.max_price, 1) >= 0.9 THEN ROUND(q.max_price / dc.max_price, 0)\n"""
+    #     query += f"""    WHEN ROUND(dc.max_price / q.max_price, 1) >= 0.9 THEN ROUND(1 / ROUND(dc.max_price / q.max_price, 0), 6)\n"""
+    #     query += f"""    ELSE 1 END AS norm_ratio\n"""
+    #     query += f"""  FROM daily_candles dc\n"""
+    #     query += f"""  INNER JOIN\n"""
+    #     query += f"""    (SELECT ticker, MIN(date_hour) AS date_hour, MAX(max_price) AS max_price\n"""
+    #     query += f"""    FROM hourly_candles\n"""
+    #     query += f"""    GROUP BY ticker, DATE(date_hour)) q\n"""
+    #     query += f"""  ON q.ticker = dc.ticker\n"""
+    #     query += f"""    AND DATE(dc.day) = DATE(q.date_hour)\n"""
+    #     query += f"""  ) n\n"""
+    #     query += f"""WHERE\n"""
+    #     query += f"""  n.ticker = \'{ticker}\'\n"""
+    #     query += f"""  AND n.ticker = hc.ticker\n"""
+    #     query += f"""  AND DATE(n.date_hour) = DATE(hc.date_hour)\n"""
+    #     query += f"""  AND n.norm_ratio <> 1;\n"""
 
-        self.insert_update(query)
+    #     self.insert_update(query)
 
-        # The last query does not correct a value in hourly_candles in which there is no corresponding day in daily_candles.
-        # So a splitted value there will not be corrected and further generate an equally not splitted daily_candle.
-        query2 = f"""UPDATE hourly_candles hc\n"""
-        query2 += f"""SET\n"""
-        query2 += f"""  open_price = ROUND(open_price / n.norm_ratio, 2),\n"""
-        query2 += f"""  max_price = ROUND(max_price / n.norm_ratio, 2),\n"""
-        query2 += f"""  min_price = ROUND(min_price / n.norm_ratio, 2),\n"""
-        query2 += f"""  close_price = ROUND(close_price / n.norm_ratio, 2)\n"""
-        query2 += f"""FROM\n"""
-        query2 += f"""  (SELECT \n"""
-        query2 += f"""    hc1.ticker, \n"""
-        query2 += f"""    hc1.open_price AS price1,\n"""
-        query2 += f"""    hc1.date_hour AS date_hour, \n"""
-        query2 += f"""    hc2.open_price AS price2,\n"""
-        query2 += f"""    hc2.date_hour AS hc2_day, \n"""
-        query2 += f"""    hc3.open_price AS price3,\n"""
-        query2 += f"""    hc3.date_hour AS hc3_day,\n"""
-        query2 += f"""    hc4.open_price AS price4,\n"""
-        query2 += f"""    hc4.date_hour AS hc4_day,\n"""
-        query2 += f"""    hc5.open_price AS price5,\n"""
-        query2 += f"""    hc5.date_hour AS hc5_day,\n"""
-        query2 += f"""    CASE \n"""
-        query2 += f"""      WHEN ROUND(hc1.close_price / hc2.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc2.open_price, 0)\n"""
-        query2 += f"""      WHEN ROUND(hc1.close_price / hc3.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc3.open_price, 0)\n"""
-        query2 += f"""      WHEN ROUND(hc1.close_price / hc4.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc4.open_price, 0)\n"""
-        query2 += f"""      WHEN ROUND(hc1.close_price / hc5.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc5.open_price, 0)\n"""
-        query2 += f"""      WHEN ROUND(hc2.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc2.open_price / hc1.close_price, 0), 6)\n"""
-        query2 += f"""      WHEN ROUND(hc3.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc3.open_price / hc1.close_price, 0), 6)\n"""
-        query2 += f"""      WHEN ROUND(hc4.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc4.open_price / hc1.close_price, 0), 6)\n"""
-        query2 += f"""      WHEN ROUND(hc5.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc5.open_price / hc1.close_price, 0), 6)\n"""
-        query2 += f"""      ELSE 1\n"""
-        query2 += f"""    END AS norm_ratio\n"""
-        query2 += f"""  FROM hourly_candles hc1\n"""
-        query2 += f"""  LEFT JOIN hourly_candles hc2 ON hc2.date_hour = hc1.date_hour + interval '1' day AND hc2.ticker = hc1.ticker\n"""
-        query2 += f"""  LEFT JOIN hourly_candles hc3 ON hc3.date_hour = hc1.date_hour + interval '2' day AND hc3.ticker = hc1.ticker\n"""
-        query2 += f"""  LEFT JOIN hourly_candles hc4 ON hc4.date_hour = hc1.date_hour + interval '3' day AND hc4.ticker = hc1.ticker\n"""
-        query2 += f"""  LEFT JOIN hourly_candles hc5 ON hc5.date_hour = hc1.date_hour + interval '4' day AND hc5.ticker = hc1.ticker\n"""
-        query2 += f"""  LEFT JOIN daily_candles dc ON dc.ticker = hc1.ticker AND dc.day = DATE(hc1.date_hour)\n"""
-        query2 += f"""  WHERE hc1.ticker = \'{ticker}\'\n"""
-        query2 += f"""    AND dc.day IS NULL) n\n"""
-        query2 += f"""WHERE\n"""
-        query2 += f"""  hc.ticker = \'{ticker}\'\n"""
-        query2 += f"""  AND hc.ticker = n.ticker\n"""
-        query2 += f"""  AND hc.date_hour = n.date_hour\n"""
-        query2 += f"""  AND n.norm_ratio <> 1;"""
+    #     # The last query does not correct a value in hourly_candles in which there is no corresponding day in daily_candles.
+    #     # So a splitted value there will not be corrected and further generate an equally not splitted daily_candle.
+    #     query2 = f"""UPDATE hourly_candles hc\n"""
+    #     query2 += f"""SET\n"""
+    #     query2 += f"""  open_price = ROUND(open_price / n.norm_ratio, 2),\n"""
+    #     query2 += f"""  max_price = ROUND(max_price / n.norm_ratio, 2),\n"""
+    #     query2 += f"""  min_price = ROUND(min_price / n.norm_ratio, 2),\n"""
+    #     query2 += f"""  close_price = ROUND(close_price / n.norm_ratio, 2)\n"""
+    #     query2 += f"""FROM\n"""
+    #     query2 += f"""  (SELECT \n"""
+    #     query2 += f"""    hc1.ticker, \n"""
+    #     query2 += f"""    hc1.open_price AS price1,\n"""
+    #     query2 += f"""    hc1.date_hour AS date_hour, \n"""
+    #     query2 += f"""    hc2.open_price AS price2,\n"""
+    #     query2 += f"""    hc2.date_hour AS hc2_day, \n"""
+    #     query2 += f"""    hc3.open_price AS price3,\n"""
+    #     query2 += f"""    hc3.date_hour AS hc3_day,\n"""
+    #     query2 += f"""    hc4.open_price AS price4,\n"""
+    #     query2 += f"""    hc4.date_hour AS hc4_day,\n"""
+    #     query2 += f"""    hc5.open_price AS price5,\n"""
+    #     query2 += f"""    hc5.date_hour AS hc5_day,\n"""
+    #     query2 += f"""    CASE \n"""
+    #     query2 += f"""      WHEN ROUND(hc1.close_price / hc2.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc2.open_price, 0)\n"""
+    #     query2 += f"""      WHEN ROUND(hc1.close_price / hc3.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc3.open_price, 0)\n"""
+    #     query2 += f"""      WHEN ROUND(hc1.close_price / hc4.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc4.open_price, 0)\n"""
+    #     query2 += f"""      WHEN ROUND(hc1.close_price / hc5.open_price, 1) >= 0.9 THEN ROUND(hc1.close_price / hc5.open_price, 0)\n"""
+    #     query2 += f"""      WHEN ROUND(hc2.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc2.open_price / hc1.close_price, 0), 6)\n"""
+    #     query2 += f"""      WHEN ROUND(hc3.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc3.open_price / hc1.close_price, 0), 6)\n"""
+    #     query2 += f"""      WHEN ROUND(hc4.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc4.open_price / hc1.close_price, 0), 6)\n"""
+    #     query2 += f"""      WHEN ROUND(hc5.open_price / hc1.close_price, 1) >= 0.9 THEN ROUND(1 / ROUND(hc5.open_price / hc1.close_price, 0), 6)\n"""
+    #     query2 += f"""      ELSE 1\n"""
+    #     query2 += f"""    END AS norm_ratio\n"""
+    #     query2 += f"""  FROM hourly_candles hc1\n"""
+    #     query2 += f"""  LEFT JOIN hourly_candles hc2 ON hc2.date_hour = hc1.date_hour + interval '1' day AND hc2.ticker = hc1.ticker\n"""
+    #     query2 += f"""  LEFT JOIN hourly_candles hc3 ON hc3.date_hour = hc1.date_hour + interval '2' day AND hc3.ticker = hc1.ticker\n"""
+    #     query2 += f"""  LEFT JOIN hourly_candles hc4 ON hc4.date_hour = hc1.date_hour + interval '3' day AND hc4.ticker = hc1.ticker\n"""
+    #     query2 += f"""  LEFT JOIN hourly_candles hc5 ON hc5.date_hour = hc1.date_hour + interval '4' day AND hc5.ticker = hc1.ticker\n"""
+    #     query2 += f"""  LEFT JOIN daily_candles dc ON dc.ticker = hc1.ticker AND dc.day = DATE(hc1.date_hour)\n"""
+    #     query2 += f"""  WHERE hc1.ticker = \'{ticker}\'\n"""
+    #     query2 += f"""    AND dc.day IS NULL) n\n"""
+    #     query2 += f"""WHERE\n"""
+    #     query2 += f"""  hc.ticker = \'{ticker}\'\n"""
+    #     query2 += f"""  AND hc.ticker = n.ticker\n"""
+    #     query2 += f"""  AND hc.date_hour = n.date_hour\n"""
+    #     query2 += f"""  AND n.norm_ratio <> 1;"""
 
-        self.insert_update(query2)
+    #     self.insert_update(query2)
 
-    def create_missing_daily_candles_from_hourly(self, ticker):
+    # def create_missing_daily_candles_from_hourly(self, ticker):
 
-        query = f"""INSERT INTO daily_candles (ticker, day, open_price, max_price, min_price, close_price, volume)\n"""
-        query += f"""SELECT agg2.ticker, agg2.date_hour AS day, agg2.open_price, agg2.max_price, agg2.min_price, hc3.close_price, agg2.volume\n"""
-        query += f"""FROM\n"""
-        query += f"""	(SELECT agg.ticker, agg.date_hour, hc2.open_price, agg.max_price, agg.min_price, agg.volume, agg.max_date_hour\n"""
-        query += f"""	FROM\n"""
-        query += f"""		(SELECT hc.ticker, DATE(hc.date_hour) AS date_hour, MIN(hc.date_hour) AS min_date_hour, MAX(hc.date_hour) AS max_date_hour, MAX(hc.max_price) AS max_price, MIN(hc.min_price) AS min_price, SUM(hc.volume) AS volume\n"""
-        query += f"""		FROM hourly_candles hc\n"""
-        query += f"""		LEFT JOIN daily_candles dc ON DATE(dc.day) = DATE(hc.date_hour)\n"""
-        query += f"""		WHERE\n"""
-        query += f"""		  dc.day IS NULL\n"""
-        query += f"""		GROUP BY hc.ticker, DATE(hc.date_hour)) agg\n"""
-        query += f"""	INNER JOIN hourly_candles hc2 ON hc2.ticker = agg.ticker and hc2.date_hour = agg.min_date_hour) agg2\n"""
-        query += f"""INNER JOIN hourly_candles hc3 ON hc3.ticker = agg2.ticker and hc3.date_hour = agg2.max_date_hour\n"""
-        query += f"""WHERE agg2.ticker = \'{ticker}\'\n"""
-        query += f"""ON CONFLICT ON CONSTRAINT daily_data_pkey DO NOTHING;"""
+    #     query = f"""INSERT INTO daily_candles (ticker, day, open_price, max_price, min_price, close_price, volume)\n"""
+    #     query += f"""SELECT agg2.ticker, agg2.date_hour AS day, agg2.open_price, agg2.max_price, agg2.min_price, hc3.close_price, agg2.volume\n"""
+    #     query += f"""FROM\n"""
+    #     query += f"""	(SELECT agg.ticker, agg.date_hour, hc2.open_price, agg.max_price, agg.min_price, agg.volume, agg.max_date_hour\n"""
+    #     query += f"""	FROM\n"""
+    #     query += f"""		(SELECT hc.ticker, DATE(hc.date_hour) AS date_hour, MIN(hc.date_hour) AS min_date_hour, MAX(hc.date_hour) AS max_date_hour, MAX(hc.max_price) AS max_price, MIN(hc.min_price) AS min_price, SUM(hc.volume) AS volume\n"""
+    #     query += f"""		FROM hourly_candles hc\n"""
+    #     query += f"""		LEFT JOIN daily_candles dc ON DATE(dc.day) = DATE(hc.date_hour)\n"""
+    #     query += f"""		WHERE\n"""
+    #     query += f"""		  dc.day IS NULL\n"""
+    #     query += f"""		GROUP BY hc.ticker, DATE(hc.date_hour)) agg\n"""
+    #     query += f"""	INNER JOIN hourly_candles hc2 ON hc2.ticker = agg.ticker and hc2.date_hour = agg.min_date_hour) agg2\n"""
+    #     query += f"""INNER JOIN hourly_candles hc3 ON hc3.ticker = agg2.ticker and hc3.date_hour = agg2.max_date_hour\n"""
+    #     query += f"""WHERE agg2.ticker = \'{ticker}\'\n"""
+    #     query += f"""ON CONFLICT ON CONSTRAINT daily_data_pkey DO NOTHING;"""
 
-        self.insert_update(query)
+    #     self.insert_update(query)
 
     def delete_weekly_candles(self, ticker):
 
@@ -260,16 +260,35 @@ class DBTickerModel:
         query += f"""FROM\n"""
         query += f"""	(SELECT agg.ticker, agg.min_day, dc2.open_price, agg.max_price, agg.min_price, agg.volume, agg.max_day\n"""
         query += f"""	FROM\n"""
-        query += f"""		(SELECT dc.ticker, DATE_PART('week', dc.day) AS week, MIN(dc.day) AS min_day, MAX(dc.day) AS max_day, \n"""
+        query += f"""		(SELECT dc.ticker, DATE_PART('year', dc.day) AS year, DATE_PART('week', dc.day) AS week, MIN(dc.day) AS min_day, MAX(dc.day) AS max_day,\n"""
         query += f"""			MAX(dc.max_price) AS max_price, MIN(dc.min_price) AS min_price, SUM(dc.volume) AS volume\n"""
         query += f"""		FROM daily_candles dc\n"""
-        query += f"""		GROUP BY dc.ticker, DATE_PART('week', dc.day)) agg\n"""
+        query += f"""		GROUP BY dc.ticker, DATE_PART('year', dc.day), DATE_PART('week', dc.day)) agg\n"""
         query += f"""	INNER JOIN daily_candles dc2 ON dc2.ticker = agg.ticker AND dc2.day = agg.min_day) agg2\n"""
         query += f"""INNER JOIN daily_candles dc3 ON dc3.ticker = agg2.ticker AND dc3.day = agg2.max_day\n"""
-        query += f"""WHERE dc3.ticker = \'{ticker}\'"""
+        query += f"""WHERE dc3.ticker = \'{ticker}\'\n"""
         query += f"""ON CONFLICT ON CONSTRAINT weekly_data_pkey DO NOTHING;"""
 
         self.insert_update(query)
+
+    def get_candles_dataframe(self, ticker, initial_date, final_date, interval='1d'):
+
+        table = 'daily_candles'
+        time_column = 'day'
+
+        if interval == '1wk':
+            table = 'weekly_candles'
+            time_column = 'week'
+
+        query = f"""SELECT ticker, {time_column}, open_price, max_price, min_price, close_price, volume\n"""
+        query += f"""FROM {table}\n"""
+        query += f"""WHERE\n"""
+
+        query += f"""  (ticker = \'{ticker}\' and {time_column} >= \'{initial_date.strftime('%Y-%m-%d')}\' and {time_column} < \'{final_date.strftime('%Y-%m-%d')}\')\n"""
+
+        query += f""";"""
+
+        return pd.read_sql_query(query, self._connection)
 
 class DBGeneralModel:
     def __init__(self):
@@ -332,9 +351,9 @@ class DBGeneralModel:
         if interval == '1wk':
             table = 'weekly_candles'
             time_column = 'week'
-        elif interval == '1h':
-            table = 'hourly_candles'
-            time_column = 'date_hour'
+        # elif interval == '1h':
+        #     table = 'hourly_candles'
+        #     time_column = 'date_hour'
 
         query = f"""SELECT ticker, {time_column}, open_price, max_price, min_price, close_price, volume\n"""
         query += f"""FROM {table}\n"""
